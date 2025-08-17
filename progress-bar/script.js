9869f52e00b881d3688edefb2e57d88bafa17fbd
Talia Crockett <tmcrockett252@gmail.com>
2025-08-17 15:42:17 -0400

    feat: adds html, css, and javascript files

---
+++ progress-bar/script.js
@@ -0,0 +1,10 @@
+let progress = 0;
+
+function increaseProgress() {
+    if (progress < 100) {
+        progress += 10;
+        document.getElementById('progressBar').style.width = progress + '%';
+    }
+}
+
+
