fe1435add0f323f526dc2250bafb493c686443a9
Talia Crockett <tmcrockett252@gmail.com>
2025-08-19 16:27:03 +0000

    feat: adds music player project

--- progress-bar/script.js
+++ progress-bar/script.js
@@ -1,20 +1,9 @@
 let progress = 0;
 
-function increaseProgressSlowly() {
-    const interval = setInterval(() => {
-        if (progress < 100) {
-            progress += 2; // Increase by 2% each time
-            document.getElementById('progressBar').style.width = progress + '%';
-        } else {
-            clearInterval(interval); // Stop when it reaches 100%
-        }
-    }, 100); // Update every 100 milliseconds
-}
+function increaseProgress() {
+  if (progress < 100) {
+    progress += 10;
+    document.getElementById("progressBar").style.width = progress + "%";
 
-function resetProgress() {
-    if (progress === 100) {
-        progress = 0;
-        document.getElementById('progressBar').style.width = '0%';
-    }
+  }
 }
-
