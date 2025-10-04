f1512f99cee9aa60af6b926ee8e8df28e87c5539
Talia Crockett <tmcrockett252@gmail.com>
2025-10-04 14:49:58 +0000

    fix: adjusts if...else syntax to properly get tracker to function

--- habit-tracker/script.js
+++ habit-tracker/script.js
@@ -6,10 +6,10 @@ const currentDay = date.getDay();
 const currentDate = date.getDate();
 const currentYear = date.getFullYear();
 
-console.log(currentMonth); 
-console.log(currentDay);
-console.log(currentDate);
-console.log(currentYear);
+console.log("The current month is " + currentMonth); 
+console.log("The current weekday is " + currentDay);
+console.log("The current date is " + currentDate);
+console.log("The current year is " + currentYear);
 
 const months = [
     "January",
@@ -40,26 +40,36 @@ habitTitle.onclick = function () {
     }
 }
 
-const daysInTheMonthList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
-const daysInTheMonth = daysInTheMonthList[currentMonth];
+const daysInTheMonthList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const daysInThisMonth = daysInTheMonthList[currentMonth];
 
 let daysCompleted = 0;
 let totalDays = document.getElementById("totalDays");
+totalDays.innerHTML = "0/" + daysInThisMonth;
+
+// Calendar Days
 
 let dayCount = 0;
 let rowCount = 0;
 let days = document.getElementsByClassName("days");
-
-for(let i =0; i < days.length; i++) {
+for(let i = 0; i < days.length; i++) {
     let day = days[rowCount].getElementsByClassName("day");
     for (let j = 0; j < day.length; j++) {
         if(dayCount == currentDate -1){
             day[j].setAttribute("style", "color:rgb(239, 162, 189)");
             day[j].setAttribute("style", "border:2px solid black");
         }
+        
+        if (dayCount < daysInThisMonth) {
+            day[j].innerHTML = dayCount + 1;
+            day[j].setAttribute("id", "day" + (dayCount + 1));
+            dayCount++;
+            } else {
+                day[j].innerHTML = "";
+                day[j].setAttribute("style", "background-color:white");
+            }
     }
+    rowCount++;
 }
 
-if (dayCount < daysInTheMonth) {
-    day[j].innerHTML = dayCount + 1;
-}
+
